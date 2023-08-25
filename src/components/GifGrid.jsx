248e0f1dcd37df4ea590d2f535823a39e69c9b50
Pablo Acosta <pablocesar087@gmail.com>
2023-08-25 16:55:17 -0400

    add folder helper

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,36 +1,34 @@
+import { useEffect, useState } from 'react';
+import { getGifs } from '../helpers/getGifts';
+
 
 export const GifGrid = ({ category }) => {
 
-    const getGifs = async() => {
-        const url = `https://api.giphy.com/v1/gifs/search?api_key=liI6jAbVbs7uYuGZKwtjpzNxIl4Eqd1V&q=${category}&limit=20`;
-        const resp = await fetch(url);
-        const { data } = await resp.json();
-
-        const gifts = data.map(img => ({
-            id: img.id,
-            title: img.title,
-            url: img.images.downsized_medium.url
-
-        }));
-
-    }
-
-    getGifs();
+    const [counter, setCounter] = useState(10);
+ 
+    useEffect( () =>  {
+        getGifs(category);
+    }, [])
 
 
     return (
         <>
-            <h3> {category} </h3>
+            <h3>{ category }</h3>
+            <h2>{ counter }</h2>
+            <button onClick={() => setCounter(counter + 1 )}>+1</button>
         </>
     ) 
 }
 
 
+/* useEffect: se utiliza para disparar efectos secundarios
+    en el useEffect siempre hay que pasar las dependencias que seria
+    el arreglo [] y significa que solo se va a ejecutar la primera vez
+    que se crea y se construye el componente
+    * Se ejecuta en 3 lugares diferentes
+        1) Cuando se monta el componente
+        2) Se ejecuta cuando hay un cambio(render) del estado
+        3) Cuando tiene alguna dependencia del cual tiene que ejecutarse
 
-// const gifts = data.map(img => {
-//     return {
-//         id: img.id,
-//         title: img.title,
-//         url: img.images.downsized_medium.url
-//     }
-// })
+    obs: El useEffect no sirve la inicializar cosas
+*/  
---
+++ src/helpers/getGifts.js
@@ -0,0 +1,26 @@
+export const getGifs = async(category) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=liI6jAbVbs7uYuGZKwtjpzNxIl4Eqd1V&q=${category}&limit=10`;
+    const resp = await fetch(url);
+    const { data } = await resp.json();
+
+    const gifts = data.map(img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+
+    }));
+
+    console.log(gifts);
+    return gifts;
+}
+
+
+
+// const gifts = data.map(img => {
+//     return {
+//         id: img.id,
+//         title: img.title,
+//         url: img.images.downsized_medium.url
+//     }
+// })
